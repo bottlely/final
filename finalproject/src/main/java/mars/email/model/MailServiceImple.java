7ee7c0bc021a00324010470c9151d9500eef0371
조병리 <@CHOBLY>
2017-11-01 14:59:37 +0900

    hi

--- finalproject/src/main/java/mars/email/model/MailServiceImple.java
+++ finalproject/src/main/java/mars/email/model/MailServiceImple.java
@@ -2,17 +2,19 @@ package mars.email.model;
 
 import javax.mail.internet.MimeMessage;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MailServiceImple implements MailService {
 	
 	private JavaMailSender javaMailSender;
-	
+
 	@Autowired
-	public MailServiceImple(JavaMailSender javaMailSender) {
-		super();
+	public void setJavaMailSender(JavaMailSender javaMailSender) {
 		this.javaMailSender = javaMailSender;
 	}
 
