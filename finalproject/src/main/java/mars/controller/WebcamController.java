0aba2113e471de3ded148e650b876b31e6b83d91
Eun Ji <kwakeunjea@naver.com>
2017-11-03 00:01:12 +0900

    webcam detect cam()

--- finalproject/src/main/java/mars/controller/WebcamController.java
+++ finalproject/src/main/java/mars/controller/WebcamController.java
@@ -1,5 +1,21 @@
 package mars.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.github.sarxos.webcam.Webcam;
+
+@Controller
 public class WebcamController {
 
+	@RequestMapping("/webMain.do")
+	public String detectCam(){
+		Webcam webcam = Webcam.getDefault();
+		if (webcam != null) {
+			System.out.println("Webcam: " + webcam.getName());
+		} else {
+			System.out.println("No webcam detected");
+		}
+		
+		return "webcam/webcamMain";
+	}
 }
