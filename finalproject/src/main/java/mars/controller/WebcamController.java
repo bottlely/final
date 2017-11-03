4abacf57b7cdd4c2a3fb2cd5629493a0a9af1c76
Eun Ji <kwakeunjea@naver.com>
2017-11-03 12:47:23 +0900

    webcam

--- finalproject/src/main/java/mars/controller/WebcamController.java
+++ finalproject/src/main/java/mars/controller/WebcamController.java
@@ -1,8 +1,10 @@
 package mars.controller;
 
+import javax.swing.JFrame;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.*;
 
 @Controller
 public class WebcamController {
@@ -12,10 +14,29 @@ public class WebcamController {
 		Webcam webcam = Webcam.getDefault();
 		if (webcam != null) {
 			System.out.println("Webcam: " + webcam.getName());
+//			playCam();
 		} else {
 			System.out.println("No webcam detected");
 		}
 		
 		return "webcam/webcamMain";
+	}
+
+	public void playCam() {
+		Webcam webcam = Webcam.getDefault();
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+
+		WebcamPanel panel = new WebcamPanel(webcam);
+		panel.setFPSDisplayed(true);
+		panel.setDisplayDebugInfo(true);
+		panel.setImageSizeDisplayed(true);
+		panel.setMirrored(true);
+
+		JFrame window = new JFrame("Test webcam panel");
+		window.add(panel);
+		window.setResizable(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);		
 	}
 }
--- finalproject/src/main/webapp/WEB-INF/views/webcam/webcamMain.jsp
+++ finalproject/src/main/webapp/WEB-INF/views/webcam/webcamMain.jsp
@@ -1,12 +1,23 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset=UTF-8">
 <title>Insert title here</title>
+<script src="https://webrtc.github.io/adapter/adapter-latest.js"></script>
+<script src="webrtc.js"></script>
 </head>
 <body>
-<h1>webcamMain.jsp</h1>
+	<h1>webcamMain.jsp</h1>
+	<video id="localVideo" autoplay muted style="width: 40%;"></video>
+	<video id="remoteVideo" autoplay style="width: 40%;"></video>
+	<br />
+
+        <input type="button" id="start" onclick="start(true)" value="Start Video"></input>
+
+        <script type="text/javascript">
+            pageReady();
+        </script>
 </body>
 </html>
