02d04cae087dad06a4a553712e976245b8c10426
Administrator <Administrator@MSDN-SPECIAL>
2017-11-08 10:45:08 +0900

    dd

--- finalproject/src/main/java/mars/controller/ReplyController.java
+++ finalproject/src/main/java/mars/controller/ReplyController.java
@@ -1,13 +1,18 @@
 package mars.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import mars.reply.model.*;
 
 @Controller
 public class ReplyController {
 
+	@Autowired
+	private ReplyDAO replydao;
+	
    @RequestMapping("reply.do")
    public ModelAndView reply(@RequestParam("reply")String reply){
       ModelAndView mav = new ModelAndView();
